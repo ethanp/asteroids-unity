8fb65a65ef4ecabeb4cb6d22a5aea760f9ff196c
Ethan Petuchowski <etahnp@gmail.com>
2019-04-27 19:56:33 -0400

    started work on ship script

---
+++ Assets/Scripts/ShipScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bulletPrefab;
+
+    private float timeGunLastFired = 0f;
+    [SerializeField]
+    private float backoffBetweenFires = .4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldFire())
+        {
+            fire();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        handleArrowKeys();
+    }
+
+    private bool shouldFire()
+    {
+
+        if (Input.GetKeyDown("space")
+            && Time.time - timeGunLastFired > backoffBetweenFires)
+        {
+            timeGunLastFired = Time.time;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void fire()
+    {
+        Instantiate(bulletPrefab);
+    }
+
+    private void handleArrowKeys()
+    {
+        if (Input.GetKeyDown("up"))
+        {
+            // TODO add `forward` force.
+        }
+        if (Input.GetKeyDown("down"))
+        {
+
+        }
+        if (Input.GetKeyDown("left"))
+        {
+
+        }
+        if (Input.GetKeyDown("right"))
+        {
+
+        }
+
+    }
+}
