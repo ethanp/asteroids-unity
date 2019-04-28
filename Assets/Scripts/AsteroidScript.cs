f60f3be9c5e704d280b93a56416b986d8ffef203
Ethan Petuchowski <etahnp@gmail.com>
2019-04-28 00:15:02 -0400

    everything working much better now.

--- Assets/Scripts/AsteroidScript.cs
+++ Assets/Scripts/AsteroidScript.cs
@@ -6,10 +6,25 @@ public class AsteroidScript : MonoBehaviour
 {
     [SerializeField] private GameObject asteroidPrefab;
 
+    private void Update()
+    {
+        // TODO destroy it if it goes off screen.
+    }
+
+    private void FixedUpdate()
+    {
+        // TODO add some force to it to make it move
+        // around. Make the speed go up AND down over time.
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("Asteroid got hit.");
+
         if (gameObject.transform.localScale.z > 1)
         {
+            Debug.Log("Creating children asteroids.");
+
             // Create children.
             GameObject leftChild = Instantiate(asteroidPrefab);
             GameObject rightChild = Instantiate(asteroidPrefab);
@@ -20,7 +35,7 @@ public class AsteroidScript : MonoBehaviour
 
             // Set position.
             leftChild.transform.position = new Vector3(
-                transform.position.x-1,
+                transform.position.x - 1,
                 transform.position.y,
                 transform.position.z);
             rightChild.transform.position = new Vector3(
