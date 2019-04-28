7964a5c686ca5e5d737e0745585847ecad62d97b
Ethan Petuchowski <etahnp@gmail.com>
2019-04-27 21:07:24 -0400

    Init asteroid/bullet collision code (not tested).

--- Assets/Scripts/AsteroidScript.cs
+++ Assets/Scripts/AsteroidScript.cs
@@ -4,10 +4,44 @@ using UnityEngine;
 
 public class AsteroidScript : MonoBehaviour
 {
+    [SerializeField] private GameObject asteroidPrefab;
+
     // TODO on collision:
     //
     // 1) destroy(this)
     //
     // 2) if bigEnough(): 
     //        instantiateChildren()
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (gameObject.transform.localScale.z > 1)
+        {
+            // Create children.
+            GameObject leftChild = Instantiate(asteroidPrefab);
+            GameObject rightChild = Instantiate(asteroidPrefab);
+
+            // Set parent.
+            leftChild.transform.parent = transform.parent;
+            rightChild.transform.parent = transform.parent;
+
+            // Set position.
+            leftChild.transform.position = new Vector3(
+                transform.position.x-1,
+                transform.position.y,
+                transform.position.z);
+            rightChild.transform.position = new Vector3(
+                transform.position.x + 1,
+                transform.position.y,
+                transform.position.z);
+
+            // Set scale.
+            Vector3 smallerScale = transform.localScale * .7f;
+            leftChild.transform.localScale = smallerScale;
+            rightChild.transform.localScale = smallerScale;
+
+            // TODO set velocity.
+        }
+        Destroy(gameObject);
+    }
 }
