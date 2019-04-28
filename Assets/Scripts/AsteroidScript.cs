0905814305fdddb5a1a2083002e9b5c28e08ce5e
Ethan Petuchowski <etahnp@gmail.com>
2019-04-27 20:55:39 -0400

    create asteroid script

---
+++ Assets/Scripts/AsteroidScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidScript : MonoBehaviour
+{
+    // TODO on collision:
+    //
+    // 1) destroy(this)
+    //
+    // 2) if bigEnough(): 
+    //        instantiateChildren()
+}
