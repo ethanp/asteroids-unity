c4968258f2a0c26e9e5b0c6c2d0ddf63f8b88a14
Ethan Petuchowski <etahnp@gmail.com>
2019-04-27 20:03:58 -0400

    Shooting works (not pretty).

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > 4)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, 1));
+    }
+}
