56244f890e5bbc4ac919d39c9e2fa110e5900f66
Ethan Petuchowski <etahnp@gmail.com>
2019-05-01 00:13:45 -0400

    camera works much better now (still not done though)

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-
     private GameObject ship_;
+    private Vector3 offset_;
 
     void Start()
     {
         ship_ = GameManager.instance.GetShip();
+        offset_ = transform.position - ship_.transform.position;
     }
 
     void Update()
     {
         GetComponent<Camera>().backgroundColor = createBackgroundColor();
 
-        transform.position = followShip();
-        transform.rotation = lookAtShip();
+        // TODO the fact that this works (got it from the Unity docs) goes to
+        // show how clueless I am here. Also, why doesn't calling
+        // `transform.LookAt(ship);` work?
+        transform.position = ship_.transform.position + offset_;
+
+        //transform.rotation = lookAtShip();
     }
 
     private Color createBackgroundColor()
@@ -31,9 +36,11 @@ public class CameraScript : MonoBehaviour
 
     private Vector3 followShip()
     {
+        Vector3 behind = ship_.transform.up * -40;
+        Vector3 above = Vector3.zero;  // ship_.transform.forward * -5;
         return Vector3.Lerp(
             a: transform.position,
-            b: ship_.transform.right * 20 + ship_.transform.up * 5,
+            b: behind + above,
             t: 1.0f);
     }
 
