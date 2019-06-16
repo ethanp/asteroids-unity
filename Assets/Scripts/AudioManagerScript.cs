306b266f469d06a556b72708322f192ffb5894f5
Ethan Petuchowski <etahnp@gmail.com>
2019-06-16 12:55:19 -0400

    add thruster sound effect slots (unused)

--- Assets/Scripts/AudioManagerScript.cs
+++ Assets/Scripts/AudioManagerScript.cs
@@ -10,18 +10,24 @@ public class AudioManagerScript : MonoBehaviour
     [SerializeField] private AudioSource userLostClip;
     [SerializeField] private AudioSource userWonClip;
     [SerializeField] private AudioSource backgroundMusicClip;
+    [SerializeField] private AudioSource forwardThrusterClip;
+    [SerializeField] private AudioSource reverseThrusterClip;
+    [SerializeField] private AudioSource turnThursterClip;
 
     // TODO Actually I want the AudioSource to be on the Ship so that the
     //      directionality is right.
     public void PlayBulletFired() { bulletFiredClip.Play(); }
-
     public void PlayShipExplosion() { shipExplosionClip.Play(); }
-
     public void PlayAsteroidExplosion() { asteroidExplosionClip.Play(); }
-
     public void PlayUserLost() { userLostClip.Play(); }
-
     public void PlayUserWon() { userWonClip.Play(); }
-
     public void PlayBackgroundMusic() { backgroundMusicClip.Play(); }
+
+    public void PlayForwardThruster() { forwardThrusterClip.Play(); }
+    public void PlayReverseThruster() { reverseThrusterClip.Play(); }
+    public void PlayTurnThruster() { turnThursterClip.Play(); }
+
+    public void StopForwardThruster() { forwardThrusterClip.Stop(); }
+    public void StopReverseThruster() { reverseThrusterClip.Stop(); }
+    public void StopTurnThruster() { turnThursterClip.Stop(); }
 }
