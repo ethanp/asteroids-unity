572c17e0e65470c4ad5ab69fb1b30f1353ca2924
Ethan Petuchowski <etahnp@gmail.com>
2019-06-16 08:25:46 -0400

    add jet particle emitters for all directions (visuals are basic)

--- Assets/Scripts/ShipScript.cs
+++ Assets/Scripts/ShipScript.cs
@@ -12,6 +12,13 @@ public class ShipScript : MonoBehaviour
     [SerializeField] private float forwardForce;
     [SerializeField] private float sideForce;
 
+    [SerializeField] private ParticleSystem leftJet;
+    [SerializeField] private ParticleSystem rightJet;
+    [SerializeField] private ParticleSystem frontJet;
+    [SerializeField] private ParticleSystem rearJet;
+    [SerializeField] private ParticleSystem upJet;
+    [SerializeField] private ParticleSystem downJet;
+
     private Rigidbody rigidbody_;
 
     void Start()
@@ -59,12 +66,47 @@ public class ShipScript : MonoBehaviour
 
     private void handleArrowKeys()
     {
-        if (Input.GetKey("up")) addForce(transform.forward * forwardForce);
-        if (Input.GetKey("down")) addForce(-transform.forward * forwardForce);
-        if (Input.GetKey("left")) addTorque(-transform.up * sideForce);
-        if (Input.GetKey("right")) addTorque(transform.up * sideForce);
-        if (Input.GetKey("w")) addTorque(-transform.right * sideForce);
-        if (Input.GetKey("s")) addTorque(transform.right * sideForce);
+        if (Input.GetKey("up"))
+        {
+            addForce(transform.forward * forwardForce);
+            if (!rearJet.isEmitting) rearJet.Play();
+        }
+        else if (rearJet.isEmitting) rearJet.Stop();
+
+        if (Input.GetKey("down"))
+        {
+            addForce(-transform.forward * forwardForce);
+            if (!frontJet.isEmitting) frontJet.Play();
+        }
+        else if (frontJet.isEmitting) frontJet.Stop();
+
+        if (Input.GetKey("left"))
+        {
+            addTorque(-transform.up * sideForce);
+            if (!rightJet.isEmitting) rightJet.Play();
+        }
+        else if (rightJet.isEmitting) rightJet.Stop();
+
+        if (Input.GetKey("right"))
+        {
+            addTorque(transform.up * sideForce);
+            if (!leftJet.isEmitting) leftJet.Play();
+        } 
+        else if (leftJet.isEmitting) leftJet.Stop();
+
+        if (Input.GetKey("w"))
+        {
+            addTorque(-transform.right * sideForce);
+            if (!downJet.isEmitting) downJet.Play();
+        }
+        else if (downJet.isEmitting) downJet.Stop();
+
+        if (Input.GetKey("s"))
+        {
+            addTorque(transform.right * sideForce);
+            if (!upJet.isEmitting) upJet.Play();
+        }
+        else if (upJet.isEmitting) upJet.Stop();
     }
 
     private void addForce(Vector3 f)
