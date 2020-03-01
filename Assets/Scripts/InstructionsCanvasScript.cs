af78fb2e3552cfa81509a64102fe813c1f69aded
Ethan Petuchowski <etahnp@gmail.com>
2020-03-01 11:39:55 -0500

    add instructions screen

---
+++ Assets/Scripts/InstructionsCanvasScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionsCanvasScript : MonoBehaviour {
+    void Update() {
+        if (Input.GetKeyDown("return")) {
+            var canvas = GetComponent<Canvas>();
+            canvas.enabled = !canvas.enabled;
+        }
+    }
+}
